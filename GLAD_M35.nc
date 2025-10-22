fb5cf1d93fa5c3a2f25fffd353798f357d488e47
tom-new <tomnewtonnew@gmail.com>
2025-10-22 14:14:03 +1100

    Add GLAD-M35

---
+++ GLAD_M35.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8937ac4fe63dbc549c5ad87c59c7a06d95cbb743fe500f2ed1a59fd4bdd950d1
+size 301323269
---
+++ processing/GLAD_M35.py
@@ -0,0 +1,118 @@
+"""python3 script to convert the GLAD-M35 tomography model to a consistently formatted netCDF4 file"""
+
+import numpy as np
+import xarray as xr
+import pandas as pd
+from pathlib import Path
+from constants import *
+
+STW105 = xr.load_dataset(Path(__file__).parent.parent / "STW105.nc")
+
+# load GLAD-M35 model
+GLAD_M35 = xr.load_dataset(Path(__file__).parent / "GLAD_M35.nc")
+for var in GLAD_M35.data_vars:
+    if var == "eta":
+        pass
+    # convert to m/s
+    GLAD_M35[var] = GLAD_M35[var] * 1e3
+
+# standardise attributes
+GLAD_M35 = GLAD_M35.rename(
+    {
+        "latitude": "lat",
+        "longitude": "lon",
+        "vsh": "Vsh",
+        "vsv": "Vsv",
+        "vph": "Vph",
+        "vpv": "Vpv",
+    }
+)
+GLAD_M35.attrs = {
+    "id": "GLAD-M35",
+    "reference": "Cui et al. (2024)",
+    "doi": "https://doi.org/10.1093/gji/ggae270",
+    "source": "Personal communication",
+}
+GLAD_M35["depth"].attrs = {"long_name": "depth", "units": "km", "positive": "down"}
+GLAD_M35["lat"].attrs = {"long_name": "latitude", "units": "degrees"}
+GLAD_M35["lon"].attrs = {
+    "long_name": "longitude",
+    "units": "degrees",
+    "convention": "bipolar",
+}
+GLAD_M35["Vph"].attrs = {
+    "long_name": "Horizontally polarised P-wave velocity",
+    "units": "m/s",
+}
+GLAD_M35["Vpv"].attrs = {
+    "long_name": "Vertically polarised P-wave velocity",
+    "units": "m/s",
+}
+GLAD_M35["Vsh"].attrs = {
+    "long_name": "Horizontally polarised S-wave velocity",
+    "units": "m/s",
+}
+GLAD_M35["Vsv"].attrs = {
+    "long_name": "Vertically polarised S-wave velocity",
+    "units": "m/s",
+}
+
+# add isotropic Vp and Vs
+GLAD_M35["Vp"] = 2 / (1 / GLAD_M35["Vpv"] + 1 / GLAD_M35["Vph"])
+GLAD_M35["Vp"].attrs = {
+    "long_name": "Isotropic P-wave velocity",
+    "units": "m/s",
+}
+GLAD_M35["Vs"] = 2 / (1 / GLAD_M35["Vsv"] + 1 / GLAD_M35["Vsh"])
+GLAD_M35["Vs"].attrs = {
+    "long_name": "Isotropic S-wave velocity",
+    "units": "m/s",
+}
+
+# add radius and make primary dim
+GLAD_M35 = GLAD_M35.assign_coords(
+    {"r": ("depth", earth_radius - GLAD_M35["depth"].data * 1e3)}
+)
+GLAD_M35["r"] = GLAD_M35["r"].assign_attrs(
+    {"long_name": "radius", "units": "m", "positive": "up"}
+)
+GLAD_M35 = GLAD_M35.swap_dims({"depth": "r"})
+GLAD_M35 = GLAD_M35.sortby("r", ascending=False)  # sort radii from cmb to surface
+
+# remove lon=180 since we have a value at lon=-180
+GLAD_M35 = GLAD_M35.isel(lon=slice(0, -1))
+
+# calculate dlnVp and dlnVs relative to STW105
+# first interpolate STW105 to GLAD_M35 radii
+STW105_i = STW105.interp(r=GLAD_M35["r"].data)
+# now calculate perturbations
+GLAD_M35["dlnVp_percent"] = (GLAD_M35["Vp"] - STW105_i["Vp"]) / STW105_i["Vp"] * 100
+GLAD_M35["dlnVs_percent"] = (GLAD_M35["Vs"] - STW105_i["Vs"]) / STW105_i["Vs"] * 100
+# standardise attributes for perturbations
+GLAD_M35["dlnVp_percent"].attrs = {
+    "long_name": "P-wave velocity perturbation",
+    "units": "percent",
+}
+GLAD_M35["dlnVs_percent"].attrs = {
+    "long_name": "S-wave velocity perturbation",
+    "units": "percent",
+}
+
+import matplotlib.pyplot as plt
+
+STW105_i["Vp"].plot(y="depth", yincrease=False)
+GLAD_M35["Vph"].mean(dim=("lat", "lon")).plot(y="depth", yincrease=False)
+plt.title("P-wave velocity")
+plt.xlabel("Velocity (m/s)")
+plt.ylabel("Depth (km)")
+plt.gca().set_ylim(2871, 0)
+plt.show()
+plt.close("all")
+
+# personally I only care for the perturbations, so drop all the other variables
+for var in GLAD_M35.data_vars:
+    if var not in ["dlnVp_percent", "dlnVs_percent"]:
+        GLAD_M35 = GLAD_M35.drop_vars(var)
+
+print(GLAD_M35)
+GLAD_M35.to_netcdf(Path(__file__).parent.parent / "GLAD_M35.nc")
